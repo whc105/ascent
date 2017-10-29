c1fd6290b64860f56cbecd348935e03f752bb7f3
Wei Huan Chen <altheagres@gmail.com>
2017-10-29 00:47:22 +0000

    Cleared a few files

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,6 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.render('index');
-    
-});
-
-
-router.get('/isLoggedIn', function(req, res, next){
-    res.send(req.session.valid);
-});
-
-router.post('/logOut', function(req, res, next) {
-    console.log("Log Out called");
-    req.session.valid = false;
-    res.send(true);
 });
 
 module.exports = router;
