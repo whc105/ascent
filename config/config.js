9f1b40897e01189f9cd8ef2b184d4815a9e26ea9
Wei Huan Chen <altheagres@gmail.com>
2017-12-02 01:09:07 +0000

    prod keys

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8080,
-    keys: process.env.NODE_ENV === 'production' ? require('./.prodKeys') : require('./.devKeys'),
+    keys: process.env.NODE_ENV === 'production' ? require('./.prodKeys') : require('./.prodKeys'),
     permissionLevels: {
         webMaster: 100,
         admin: 3,
