f3838194f11f7456e63d00ee92b9d23291d7073d
Wei Huan Chen <altheagres@gmail.com>
2017-12-02 01:12:01 +0000

    changes

---
+++ config/.devKeys.js
@@ -0,0 +1,10 @@
+const mongoUser = 'ascent_db_internal';
+const mongoPass = '3wJcoIjfeJ9RfhQC';
+
+module.exports = {
+  googleClientID: '429195341441-ba9vq5keo8vbn04il3sjvs28n0ebonmp.apps.googleusercontent.com',
+  googleClientSecret: 'snl5hr3ASzYSSaHyuGc4hd0x',
+  mongoURI: `mongodb://${mongoUser}:${mongoPass}@ds113835.mlab.com:13835/ascent_db`,
+  sessionKey: 'asdf',
+  cookieKey: 'dfgsdfgjsldfkjg shg8ehs iulhfgh ao87wegisuyerhf sdjfg ehiyhgdf9ui g',
+};
